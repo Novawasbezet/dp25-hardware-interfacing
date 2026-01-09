31df15daefbb58c2f7524861f0a6e7836e9ba435
Nova Bank (student) <nova.bank@windesheim.nl>
2026-01-09 19:28:38 +0100

    Statuslogica en tijd toegevoegd

--- Hardware/main.py
+++ Hardware/main.py
@@ -10,21 +10,49 @@ led_rood = Pin(12, Pin.OUT)
 tiltsensor = Pin(4, Pin.IN, Pin.PULL_UP)
 reset_knop = Pin(14, Pin.IN, Pin.PULL_UP)
 
+# Tijdgrenzen
+RISICO_TIJD = 3000
+GEVAAR_TIJD = 5000
+
+status = "VEILIG"
+start_afwijking = None
+
 # Start veilig
 led_groen.value(1)
-led_geel.value(0)
-led_rood.value(0)
 
 print("Systeem gestart, status veilig")
 
 while True:
-    if tiltsensor.value() == 1:
-        print("Afwijking gedetecteerd")
-        led_groen.value(0)
-        led_geel.value(1)
+    nu = time.ticks_ms()
 
-    if reset_knop.value() == 0:
+    if tiltsensor.value() == 1:
+        if start_afwijking is None:
+            start_afwijking = nu
+
+        duur = time.ticks_diff(nu, start_afwijking)
+
+        if duur >= GEVAAR_TIJD:
+            status = "GEVAAR"
+            led_rood.value(1)
+            led_geel.value(0)
+            led_groen.value(0)
+
+        elif duur >= RISICO_TIJD:
+            status = "RISICO"
+            led_geel.value(1)
+            led_groen.value(0)
+
+    else:
+        start_afwijking = None
+        status = "VEILIG"
         led_groen.value(1)
         led_geel.value(0)
+        led_rood.value(0)
 
-    time.sleep(0.2)
+    if status == "GEVAAR" and reset_knop.value() == 0:
+        status = "VEILIG"
+        start_afwijking = None
+        led_groen.value(1)
+        led_rood.value(0)
+
+    time.sleep(0.1)
