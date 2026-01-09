bef50a27eb02cc4a9c4aa652a15e58b06647f9aa
Nova Bank (student) <nova.bank@windesheim.nl>
2026-01-09 19:25:07 +0100

    Tiltsensor en resetknop werkend gemaakt

--- Hardware/main.py
+++ Hardware/main.py
@@ -6,7 +6,11 @@ led_groen = Pin(10, Pin.OUT)
 led_geel = Pin(11, Pin.OUT)
 led_rood = Pin(12, Pin.OUT)
 
-# Start in veilige toestand
+# Inputs
+tiltsensor = Pin(4, Pin.IN, Pin.PULL_UP)
+reset_knop = Pin(14, Pin.IN, Pin.PULL_UP)
+
+# Start veilig
 led_groen.value(1)
 led_geel.value(0)
 led_rood.value(0)
@@ -14,4 +18,13 @@ led_rood.value(0)
 print("Systeem gestart, status veilig")
 
 while True:
-    time.sleep(0.5)
+    if tiltsensor.value() == 1:
+        print("Afwijking gedetecteerd")
+        led_groen.value(0)
+        led_geel.value(1)
+
+    if reset_knop.value() == 0:
+        led_groen.value(1)
+        led_geel.value(0)
+
+    time.sleep(0.2)
